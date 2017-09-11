380a5918439169f1a411e733633a9d51fe6271e2
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-09-11 23:58:37 +0200

    Improve deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-docker-compose run plum mix dialyzer --halt-exit-status
-docker-compose run test
+docker-compose run --rm plum mix dialyzer --halt-exit-status
+docker-compose run --rm test
+eval "$( aws ecr get-login --no-include-email --region eu-west-1 )"
 mix docker.shipit
 mix docker.publish --tag latest
 ssh -i ~/.ssh/aws-eb4 ec2-user@34.253.234.153 'bash -s' < remote_deploy.sh
--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set +e
+eval "$( aws ecr get-login --no-include-email --region eu-west-1 )"
 docker pull 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest
 docker stop $(docker ps -a -q)
-docker run -p 80:4000 --env-file .env 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest foreground
 docker rm $(docker ps -a -q)
+docker run -p 80:4000 --env-file .env 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest foreground
