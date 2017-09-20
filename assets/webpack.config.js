64ebe1329e659aa9279c2b6d9bbc1ff13a865d88
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-09-20 22:51:15 +0200

    Use webpack instead of brunch

---
+++ assets/webpack.config.js
@@ -0,0 +1,81 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+
+config = {
+  entry: [
+    './js/app.js',
+  ],
+  output: {
+    path: path.join(__dirname, '..', 'priv', 'static'),
+    filename: 'js/app.js'
+  },
+  module: {
+    rules: [{
+      // test: /\.png$|\.jpg$/,
+      // loader: 'url-loader',
+    // }, {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['env']
+      }
+    }, {
+      test: /\.scss$/,
+      exclude: [/node_modules/],
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader?sourceMap',
+          {
+            loader: 'sass-loader?sourceMap',
+            options: {
+              includePaths: [
+                path.resolve(__dirname, 'node_modules/bootstrap/scss'),
+                path.resolve(__dirname, 'node_modules/font-awesome/scss'),
+                path.resolve(__dirname, 'node_modules/mdbootstrap/sass')
+              ]
+            }
+          }
+        ]
+      })
+    }, {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        use: 'css-loader?sourceMap'
+      })
+    }, {
+      test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+      loader: 'url-loader'
+    }]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      path.join(__dirname, 'js'),
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({filename: 'css/app.css'}),
+    new CopyWebpackPlugin([{from: './static'}, {from: './node_modules/mdbootstrap/font/roboto', to: 'fonts'}]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
+};
+
+if (process.env.ENV === 'production') {
+  config.plugins = config.plugins.concat([
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+    })
+  ]);
+}
+
+module.exports = config;
