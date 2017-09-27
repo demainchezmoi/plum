578faf4ef56501c1cdee5486286e78c3f4612f72
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-09-27 20:34:20 +0200

    Deploy from inside container

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 set -e
 # docker-compose run --rm plum mix dialyzer --halt-exit-status
 docker-compose run --rm test
-eval "$( aws ecr get-login --no-include-email --region eu-west-1 )"
-mix docker.shipit
-mix docker.publish --tag latest
+docker-compose run --rm plum /bin/bash -c 'wrapdocker \
+	& eval "$( aws ecr get-login --no-include-email --region eu-west-1 )" \
+	&& mix do docker.shipit, docker.publish --tag latest'
+
 ssh -i ~/.ssh/aws-eb4 ec2-user@54.194.151.97 'bash -s' < remote_deploy.sh
