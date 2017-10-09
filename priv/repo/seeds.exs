5fdac25217b8d978783e073b73a2add9e8581561
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-10-09 10:55:13 +0200

    Add seed create land and ad

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+{:ok, land} = Plum.Sales.create_land(%{lat: 4.81, lng: 2.3, description: "Beau terrain arboré de 1000 m² avec vue imprenable sur le vexin situé à lainville en vexin sur rue très calme. cos 0.2 et façade de 16m. beau potentiel, a voir rapidement avec l'adresse meulan.", images: ["https://s3-eu-west-1.amazonaws.com/demainchezmoi/cloudfront_assets/images/terrain.jpg"], city: "Blaru", department: "78", price: 39_800, surface: 421})
+
+{:ok, ad} = Plum.Sales.create_ad(%{active: true, land_id: land.id})
