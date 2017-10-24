0cefd9a9903c30a9dd65eaddd6a06c194e441478
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-10-24 14:28:28 +0200

    mixpanel identify set id

--- assets/js/helpers/mixpanel.js
+++ assets/js/helpers/mixpanel.js
@@ -12,7 +12,8 @@ export function identify(user) {
     mixpanel.people.set({
       $email: user.email,
       $first_name: user.first_name,
-      $last_name: user.last_name
+      $last_name: user.last_name,
+      id: user.id
     });
   }
 }
