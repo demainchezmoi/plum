3a2908dc72a38f03f20daa15ed152bcda3c2f880
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-10-24 15:11:14 +0200

    Add logging to just inserted plug

--- lib/plum_web/plugs/just_inserted_user.ex
+++ lib/plum_web/plugs/just_inserted_user.ex
@@ -1,15 +1,19 @@
 defmodule PlumWeb.Plugs.JustInsertedUser do
   import Plug.Conn
+  require Logger
 
   def init(_), do: :ok
 
   def call(conn, _) do
     case conn |> get_session(:_plum_user_status) do
       :inserted ->
+        Logger.info "Just insterted detected"
         conn
         |> assign(:user_just_inserted, true)
         |> put_session(:_plum_user_status, nil)
-      _ ->
+
+      e ->
+        Logger.info "Just insterted NOT detected #{inspect e}"
         conn
     end
   end
