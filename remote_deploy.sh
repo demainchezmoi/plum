b7a738e469880f3a49456066c220dc7f522870d6
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-11-13 14:09:39 +0100

    Forward port 443 to container

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -4,4 +4,4 @@ eval "$( aws ecr get-login --no-include-email --region eu-west-1 )"
 docker pull 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker run -p 80:4000 --env-file .env 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest foreground
+docker run -p 80:4000 -p 443:4000 --env-file .env 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest foreground
