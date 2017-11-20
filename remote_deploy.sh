3201e8ee8273c3f1cbf8eb53b6c99f8321287cfc
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-11-20 15:55:35 +0100

    Remote deploy fix remove containers and images

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -2,7 +2,7 @@
 set +e
 eval "$( aws ecr get-login --no-include-email --region eu-west-1 )"
 docker pull 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest
-docker ps -q | xargs docker stop
-docker ps -q | xargs docker rm
-docker images -f "dangling=true" -q | xargs docker rmi
+docker ps -qa | xargs docker stop
+docker ps -qa | xargs docker rm
+docker images -f "dangling=true" -qa | xargs docker rmi
 docker run -p 80:4000 --env-file .env -v /home/ec2-user/.google-auth.json:/opt/app/.google-auth.json 383646808490.dkr.ecr.eu-west-1.amazonaws.com/demainchezmoi/plum:latest foreground
