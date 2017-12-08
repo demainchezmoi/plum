bd2f814feeebc18d104c70813a7369dd859524ca
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-12-08 20:49:28 +0100

    Wip

--- assets/js/views/page/index.js
+++ assets/js/views/page/index.js
@@ -1,5 +1,6 @@
 import MainView from '../main';
 import GoogleMapsLoader from 'google-maps';
+import {track} from '../../helpers/mixpanel';
 
 GoogleMapsLoader.KEY = 'AIzaSyDKvX-CXe8vAoyx69fEk91EmGIyiYydoC0';
 GoogleMapsLoader.LIBRARIES = ['places'];
@@ -9,14 +10,25 @@ module.exports = class View extends MainView {
   constructor() {
     super();
     this.google = null;
+    this.stuck = false;
     this.initMap = this.initMap.bind(this);
+    this.setupSticky = this.setupSticky.bind(this);
+    this.getDistance = this.getDistance.bind(this);
   }
 
   mount() {
     super.mount();
-    this.place = null;
+    this.h = document.getElementById("ad_stripe");
+    this.setupSticky();
+
+    if ((typeof window.ad) !== "undefined") {
+      track('VISIT_AD', {ad: window.ad});
+    } else {
+      track('VISIT_INDEX');
+    }
 
     const lazy = $("img[data-src]");
+    const input = $('[data-action=search-location]');
 
     lazy.each(function() { this.setAttribute("src", this.getAttribute('data-src')); });
     lazy.removeAttr("data-src");
@@ -27,9 +39,8 @@ module.exports = class View extends MainView {
     });
 
     $('[data-action=find-me-a-land]').click(function() {
-      const input = $('[data-action=search-location]').val();
       $('#contactModal').modal('show');
-      $('#contact_remark').val(`Je souhaite habiter à ${input}`);
+      $('#contact_remark').val(`Je souhaite habiter à ${input.val()}`);
     });
   }
 
@@ -42,30 +53,31 @@ module.exports = class View extends MainView {
     const autocompleteOptions = {componentRestrictions: {country: 'fr'}};
     const autocomplete = new this.google.maps.places.Autocomplete(input, autocompleteOptions);
   }
-}
 
+  getDistance() {
+    const topDist = this.h.offsetTop;
+    return topDist;
+  }
 
-var h = document.getElementById("ad_stripe");
-var stuck = false;
-var stickPoint = getDistance();
-
-function getDistance() {
-  var topDist = h.offsetTop;
-  return topDist;
-}
-
-window.onscroll = function(e) {
-  var distance = getDistance() - window.pageYOffset;
-  var offset = window.pageYOffset;
-  if ( (distance <= 0) && !stuck) {
-    h.style.position = 'fixed';
-    h.style.top = '0px';
-    h.style.right = '0px';
-    h.style.left = '0px';
-    h.style['z-index'] = '1000';
-    stuck = true;
-  } else if (stuck && (offset <= stickPoint)){
-    h.style.position = 'static';
-    stuck = false;
+  setupSticky() {
+    if (!!this.h) {
+      const stickPoint = this.getDistance();
+      window.onscroll = (e) => {
+        const distance = this.getDistance() - window.pageYOffset;
+        const offset = window.pageYOffset;
+        if ((distance <= 0) && !this.stuck) {
+          this.h.style.position = 'fixed';
+          this.h.style.top = '0px';
+          this.h.style.right = '0px';
+          this.h.style.left = '0px';
+          this.h.style['z-index'] = '10';
+          this.stuck = true;
+        } else if (this.stuck && (offset <= stickPoint)){
+          this.h.style.position = 'absolute';
+          this.stuck = false;
+          this.h.style = '';
+        }
+      }
+    }
   }
 }
