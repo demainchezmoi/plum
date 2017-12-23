9dea1faf03f394568cbc90bcd7c3183dec7c3b66
Alexandre Hervé <alexandre.herve9@gmail.com>
2017-12-23 16:57:14 +0100

    Update ping api

--- lib/plum_web/controllers/api/ping_controller.ex
+++ lib/plum_web/controllers/api/ping_controller.ex
@@ -4,6 +4,6 @@ defmodule PlumWeb.Api.PingController do
   action_fallback PlumWeb.FallbackController
 
   def ping(conn, _params) do
-    conn |> send_resp(200, Poison.encode!(%{body: "pong"}))
+    conn |> send_resp(200, Poison.encode!(%{body: %{ping: "pong"}}))
   end
 end
