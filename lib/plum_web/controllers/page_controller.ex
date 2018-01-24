597727d6ef2af96d565a715e740d5da7549f652c
Alexandre Hervé <alexandre.herve9@gmail.com>
2018-01-24 13:26:45 +0100

    Format us phone for zapier -> slack

--- lib/plum_web/controllers/page_controller.ex
+++ lib/plum_web/controllers/page_controller.ex
@@ -26,7 +26,7 @@ defmodule PlumWeb.PageController do
     conn =
       if not is_undef(contact_params, "phone") do
         creation = NaiveDateTime.utc_now |> NaiveDateTime.to_iso8601
-        Plum.Zapier.new_prospect(contact_params |> Map.put("creation", creation))
+        Plum.Zapier.new_prospect(contact_params |> Map.put("creation", creation) |> format_phone_us)
         Sales.create_prospect_from_contact(contact_params)
         conn |> put_flash(:info, "Votre demande a bien été prise en compte.")
       else
@@ -34,6 +34,23 @@ defmodule PlumWeb.PageController do
       end
     conn |> redirect(to: page_path(conn, :index))
   end
+  
+  def format_phone_us(params = %{"phone" => phone}) do
+    us_phone =
+      phone
+      |> String.replace(" ", "")
+      |> String.replace("-", "")
+      |> String.replace(".", "")
+      |> String.replace(",", "")
+      |> String.split("", trim: true)
+      |> Enum.with_index
+      |> Enum.reduce("", fn ({val, index}, acc) ->
+        prepended = if index in [3, 6], do: "-", else: ""
+        "#{acc}#{prepended}#{val}"
+      end)
+    params |> Map.put("phone", "#{phone} (#{us_phone})")
+  end
+  def format_phone_us(params), do: params
 
   def is_undef(params, field), do: params[field] == "" or is_nil(params[field])
 end
