74d2c35fcaeab0c08589593339507d56b9244b21
Alexandre Hervé <alexandre.herve9@gmail.com>
2018-01-30 13:22:30 +0100

    Augment page sizes

--- lib/plum/repo.ex
+++ lib/plum/repo.ex
@@ -1,6 +1,6 @@
 defmodule Plum.Repo do
   use Ecto.Repo, otp_app: :plum
-  use Scrivener, page_size: 15
+  use Scrivener, page_size: 25
 
   @doc """
   Dynamically loads the repository url from the
