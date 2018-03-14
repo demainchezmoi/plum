4a98658dc126618c04fb641e578bb8c9724eb7da
Alexandre Hervé <alexandre.herve9@gmail.com>
2018-03-14 15:42:40 +0100

    Wip

--- assets/js/views/page/index.js
+++ assets/js/views/page/index.js
@@ -14,14 +14,15 @@ module.exports = class View extends MainView {
     this.google = null;
     this.stuck = false;
     this.initMap = this.initMap.bind(this);
-    this.setupSticky = this.setupSticky.bind(this);
+    // this.setupSticky = this.setupSticky.bind(this);
     this.getDistance = this.getDistance.bind(this);
+    this.setupSmoothScroll();
   }
 
   mount() {
     super.mount();
     this.h = document.getElementById("ad_stripe");
-    this.setupSticky();
+    // this.setupSticky();
 
     if ((typeof window.ad) !== "undefined") {
       track('VISIT_AD', {ad: window.ad});
@@ -82,17 +83,20 @@ module.exports = class View extends MainView {
   }
 
   initMap() {
-    const input = $('[data-action=search-location]')[0];
+    const inputs = $('[data-action=search-location]');
     const cityInput = $('[data-role=searched-city-hidden-input]');
     const postalCodeInput = $('[data-role=searched-postal-code-hidden-input]');
     const autocompleteOptions = {componentRestrictions: {country: 'fr'}, types: ['(cities)']};
-    const autocomplete = new this.google.maps.places.Autocomplete(input, autocompleteOptions);
-    autocomplete.addListener('place_changed', () => {
-      const place = autocomplete.getPlace();
-      const postalCode = this.findPostalCode(place.address_components);
-      const city = this.findCity(place.address_components);
-      cityInput.val(city);
-      postalCodeInput.val(postalCode);
+    inputs.each((i, input) => {
+      const autocomplete = new this.google.maps.places.Autocomplete(input, autocompleteOptions);
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        const postalCode = this.findPostalCode(place.address_components);
+        const city = this.findCity(place.address_components);
+        inputs.val(place.formatted_address);
+        // cityInput.val(city);
+        // postalCodeInput.val(postalCode);
+      });
     })
   }
 
@@ -101,25 +105,86 @@ module.exports = class View extends MainView {
     return topDist;
   }
 
-  setupSticky() {
-    if (!!this.h) {
-      const stickPoint = this.getDistance();
-      window.onscroll = (e) => {
-        const distance = this.getDistance() - window.pageYOffset;
-        const offset = window.pageYOffset;
-        if ((distance <= 0) && !this.stuck) {
-          this.h.style.position = 'fixed';
-          this.h.style.top = '0px';
-          this.h.style.right = '0px';
-          this.h.style.left = '0px';
-          this.h.style['z-index'] = '10';
-          this.stuck = true;
-        } else if (this.stuck && (offset <= stickPoint)){
-          this.h.style.position = 'absolute';
-          this.stuck = false;
-          this.h.style = '';
+  setupSmoothScroll() {
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 400, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
         }
-      }
-    }
+      });
   }
+
+  // setupSticky() {
+  // const image = $('#leo_contour');
+  // const house = $('#house');
+	// const top = image.offset().top;
+	// const width = image.width();
+	// const height = image.height();
+	// const bottom = house.offset().top + house.height();
+	// const windowHeight = $(window).height();
+	// const offset = (windowHeight - height) / 2;
+
+	// const setImage = () => {
+	// const y =  $(window).scrollTop() + offset;
+	// if (y > top && y + height < bottom) {
+	// image
+	// .removeClass('absolute')
+	// .css('bottom', '')
+	// .addClass('fixed')
+	// .css('top', offset)
+	// .width(width)
+	// .height(height);
+	// }
+	// else if (y + height >= bottom) {
+  // image
+  // .addClass('absolute')
+  // .removeClass('fixed')
+  // .css('top', '')
+  // .css('bottom', 0)
+  // .width(width)
+  // .height(height);
+  // }
+	// else {
+	// image
+	// .removeClass('fixed')
+	// .removeClass('absolute')
+	// .css('top', '')
+	// .css('bottom', '')
+	// .width('')
+	// .height('');
+	// }
+	// }
+
+	// $(document).on('scroll', setImage);
+	// setImage();
+  // }
 }
